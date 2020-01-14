7112e7a08c89c5187fdab1b805cbf0596c566e74
ilya_kho <ilya.khozaynov@gmail.com>
2020-01-14 20:53:27 +0300

    Есть только первый фильтр. Нужно стилизовать

---
+++ src/components/homePage/homePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomePage =()=>{
+    return(
+        <div>
+            <h3>Давайте выберем вам тур всего за 3 этапа.</h3>
+            <div>
+
+
+            </div>
+
+
+          
+        </div>
+    )
+}
+
+export default HomePage
